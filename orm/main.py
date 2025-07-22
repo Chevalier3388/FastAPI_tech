94b18062c552a3c11d7c3d8cea8e4f45b6038812
Chevalier88 <chevalier88@icloud.com>
2025-07-22 13:38:27 +0300

    orm

--- orm/main.py
+++ orm/main.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, Depends
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
@@ -38,16 +38,19 @@ async def setup_database():
         await conn.run_sync(Base.metadata.create_all)
     return {"Tables": "successfully created"}
 
-class BookAddShema(BaseModel):
+class BookAddSсhema(BaseModel):
     title: str
     author: str
 
-class BookShema(BookAddShema):
+class BookSсhema(BookAddSсhema):
     id: int
+
+class BookResponseSсhema(BaseModel):
+    msg: str
 
 
 @app.post("/books")
-async def add_book(data: BookAddShema, session: SessionDep):
+async def add_book(data: BookAddSсhema, session: SessionDep) -> BookResponseSсhema:
     new_book = BookModel(
         title=data.title,
         author=data.author,
@@ -56,8 +59,21 @@ async def add_book(data: BookAddShema, session: SessionDep):
     await session.commit()
     return {"msg": "successful book addition"}
 
+class PaginationParams(BaseModel):
+    limit: int = Field(5, ge=0, le=100, description="Количество элем на страницу")
+    offset: int = Field(0, ge=0, description="Смещение для пагинации")
+
+PaginationDep = Annotated[PaginationParams, Depends(PaginationParams)]
+
 @app.get("/books")
-async def get_books(session: SessionDep):
-    query = select(BookModel)
+async def get_books(
+        session: SessionDep,
+        pagination: PaginationDep,
+        ) -> list[BookSсhema]:
+    query = (
+        select(BookModel)
+        .limit(pagination.limit)
+        .offset(pagination.offset)
+    )
     result = await session.execute(query)
     return result.scalars().all()
